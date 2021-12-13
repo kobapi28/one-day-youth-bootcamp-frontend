4dbdc8f49961bd84e7f472a73a8b35ffce48ba41
Shoma Kobayashi <sho1228.k@gmail.com>
2021-12-13 16:18:02 +0900

    update: finish tutorial

--- course/react-typescript/src/components/TaskList.tsx
+++ course/react-typescript/src/components/TaskList.tsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import React from "react";
+import { Task } from "..";
 
-export const TaskList: React.FC = () => {
+type Props = {
+  tasks: Task[];
+  setTasks: React.Dispatch<React.SetStateAction<Task[]>>;
+};
+
+export const TaskList: React.FC<Props> = ({ tasks, setTasks }) => {
   // Taskの状態を切り替える
+  const handleCheckBox = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    i: number
+  ) => {
+    // ミュータブル・イミュータブルの話？
+    // indexでtasks[index]とかしてしまうと直接書き換えしてしまう
+    const newTasks = tasks.map((task, _i) => {
+      return _i === i ? { ...task, isDone: e.target.checked } : task;
+    });
+    setTasks(newTasks);
+  };
 
   return (
-    <></>
+    <ul>
+      {tasks.map((task, index) => (
+        <li key={`todo-${index}`}>
+          {task.isDone ? <s>{task.label}</s> : task.label}
+          <input
+            onChange={(e) => handleCheckBox(e, index)}
+            type="checkbox"
+            checked={task.isDone}
+          />
+        </li>
+      ))}
+    </ul>
   );
 };
--- course/react-typescript/src/index.tsx
+++ course/react-typescript/src/index.tsx
@@ -1,28 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
-import { TaskList } from './components/TaskList';
-import { TaskForm } from './components/TaskForm';
+import { TaskList } from "./components/TaskList";
+import { TaskForm } from "./components/TaskForm";
+import { request } from "./server";
 
 // TODOタスクの型
-export type Task = {}
+export type Task = {
+  label: string;
+  isDone: boolean;
+};
 
 const App: React.VFC = () => {
   // タスクリストを格納する
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   // 追加前のタスクを格納する
+  // この状態ではlabelのみの管理
+  const [newTaskLabel, setNewTaskLabel] = useState<string>("");
 
   // ページマウント時にモックAPIからデータを取得
+  useEffect(() => {
+    request.fetchTasks((payload: Task[]) => setTasks(payload));
+  }, []);
 
   return (
     <div>
       {/* ヘッダー */}
       <h1>Tutorial Works</h1>
+      <h2>React Todo List</h2>
 
       {/* 一覧表示 */}
-      <TaskList />
+      <TaskList {...{ tasks, setTasks }} />
 
       {/* タスク追加、削除 */}
-      <TaskForm />
+      <TaskForm {...{ tasks, setTasks, newTaskLabel, setNewTaskLabel }} />
     </div>
   );
 };
